731f5380a613deeff3be8ecfcfa92c0a759f9ad7
Lam Le <15520405@gm.uit.edu.vn>
2019-01-12 00:40:43 +0700

    Implemented course panels

--- Programmio/Assets/Scripts/NodeScript.cs
+++ Programmio/Assets/Scripts/NodeScript.cs
@@ -13,4 +13,12 @@ public class NodeScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+	public void EnableCourseInformation() {
+        transform.parent.parent.Find("Overlay").gameObject.SetActive(true);
+        transform.parent.parent.Find("CoursePanel").gameObject.SetActive(true);
+        transform.parent.parent.Find("CoursePanel").Find("CourseOverview").gameObject.SetActive(true);
+        transform.parent.parent.Find("CoursePanel").Find("CourseList").gameObject.SetActive(false);
+    }
 }
---
+++ Programmio/Assets/Scripts/StartLearningController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLearningController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject CourseOverview;
+    public GameObject CourseList;
+    public void SwitchToCourseList()
+    {
+        CourseOverview.SetActive(false);
+        CourseList.SetActive(true);
+    }
+}
