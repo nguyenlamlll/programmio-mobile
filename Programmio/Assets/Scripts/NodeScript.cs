e94ef635371e0484e783df24f04094269a54a139
Lam Le <15520405@gm.uit.edu.vn>
2019-01-11 23:08:23 +0700

    Implementing main scenes

---
+++ Programmio/Assets/Scripts/NodeScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
